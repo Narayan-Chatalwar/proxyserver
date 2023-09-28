5d24c71be665db70a478a43fb37e6713b5b276b7
narayan <narayanchatalwar@gmail.com>
2023-09-28 12:54:09 +0530

    backend repository created

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const axios = require("axios");
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/maindata", (req, res) => {
+  axios
+    .get(
+      "https://main--mywebsite--narayan-chatalwar.hlx.live/dataforindex.json"
+    )
+    .then((result) => res.send(result.data))
+    .catch((err) => res.send(err));
+});
+
+app.get("/footerdata", (req, res) => {
+  axios
+    .get(
+      "https://main--mywebsite--narayan-chatalwar.hlx.live/dataforfooter.json"
+    )
+    .then((result) => res.send(result.data))
+    .catch((err) => res.send(err));
+});
+
+app.listen("8080", () => {
+  console.log("listening on port 8080");
+});
